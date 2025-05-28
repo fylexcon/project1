b4945bcb03f5d9f53e931abc461b37d94cfd41cc
Selim <141612172+fylexcon@users.noreply.github.com>
2025-05-28 12:37:59 +0300

    readme changed

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-        <h1>Merhabalar React!</h1>
+        <h1>aşkın olayım</h1>
         </p>
         <a
           className="App-link"
